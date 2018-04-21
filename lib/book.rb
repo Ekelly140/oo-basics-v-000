1e5a1ffd0e0bb2af6a473f75b66801da8dc790d8
ERIC KELLY <eakelly220@gmail.com>
2018-04-21 16:56:41 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,7 +1,18 @@
 
 class Book
   def initialize(title)
+    @title = title
+  end 
+  def title
+    @title
+  end 
+
+
+  def author=(autor)
+   @author = autor 
+  end 
+  def author
+    @author 
   end 
 end 
-
 Book.new("And Then THere Were None")
